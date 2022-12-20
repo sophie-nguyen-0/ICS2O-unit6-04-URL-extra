35f093107950ab62b85c2d657ecb0c74707d0a36
sophie-nguyen-0 <sophie-nguyen-0@users.noreply.github.com>
2022-12-20 19:43:53 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -16,18 +16,19 @@ if (navigator.serviceWorker) {
 /**
  * this function
  */
-window.onload = function() {
+window.onload = function () {
   const params = new URLSearchParams(document.location.search)
 
-  const ans = params.get('ans')
+  const ans = params.get("ans")
 
   if (ans == "cats" || ans == "cat") {
-    document.getElementById('answer').innerHTML = '<h3> you like cats! </h3> <br><img src = "./images/cat.jpeg" alt = "cat"> <br> <h4> fun fact! Cats spend 70% of their lives sleeping! </h4>'
-  }
-  else if (ans=="dogs" || ans=="dog") {
-    document.getElementById('answer').innerHTML = '<h3> you like dogs! </h3> <br> <img src = "./images/dog.png" alt = "dog" width="25%" height="25%"> <br> <h4> fun fact! Dogs possess only two types of cones and can only discern blue and yellow - this limited color perception is called dichromatic vision.</h4>'
-  }
-  else {
-    document.getElementById('answer').innerHTML = '<h4>please choose cats or dog. nothing else.</h4> <br> <img src = "./images/madPikachu.jpg" alt="mad pikachu" width="25%" height="25%"> <p> you made pikachu mad </p>'
+    document.getElementById("answer").innerHTML =
+      '<h3> you like cats! </h3> <br><img src = "./images/cat.jpeg" alt = "cat"> <br> <h4> fun fact! Cats spend 70% of their lives sleeping! </h4>'
+  } else if (ans == "dogs" || ans == "dog") {
+    document.getElementById("answer").innerHTML =
+      '<h3> you like dogs! </h3> <br> <img src = "./images/dog.png" alt = "dog" width="25%" height="25%"> <br> <h4> fun fact! Dogs possess only two types of cones and can only discern blue and yellow - this limited color perception is called dichromatic vision.</h4>'
+  } else {
+    document.getElementById("answer").innerHTML =
+      '<h4>please choose cats or dog. nothing else.</h4> <br> <img src = "./images/madPikachu.jpg" alt="mad pikachu" width="25%" height="25%"> <p> you made pikachu mad </p>'
   }
 }
